794de3bf33ed0c18bfdf4d94cf27d10ffbf37346
Виталий Максимов <2california@mail.ru>
2019-03-22 05:06:12 +0600

    Комнаты но без слайдера

---
+++ src/js/main.js
@@ -0,0 +1,38 @@
+		// $(document).ready(function () {
+		// 	$('.rooms-single').slick({
+		// 		  slidesToShow: 1,
+		// 		  slidesToScroll: 1,
+		// 		  arrows: false,
+		// 			fade: true,
+		// 			asNavFor: '.rooms-nav'
+		// 	});
+		// 	$('.rooms-nav').slick({
+		// 		slidesToShow: 3,
+		// 		slidesToScroll: 1,
+		// 		asNavFor: '.rooms-single',
+		// 		dots: true,
+		// 		centerMode: true,
+		// 		focusOnSelect: true
+		// 	});
+		// });
+
+
+
+// $(document).ready(function () {
+// 	$('.slider-for').slick({
+// 		slidesToShow: 1,
+// 		slidesToScroll: 1,
+// 		arrows: false,
+// 		fade: true,
+// 		asNavFor: '.slider-nav'
+// 	});
+// 	$('.slider-nav').slick({
+// 		slidesToShow: 3,
+// 		slidesToScroll: 1,
+// 		asNavFor: '.slider-for',
+// 		dots: true,
+// 		centerMode: true,
+// 		focusOnSelect: true
+// 	});
+
+// });
