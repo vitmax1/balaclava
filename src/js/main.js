bd55e994064613059864946fe867483ae2ce3325
Виталий Максимов <2california@mail.ru>
2019-03-23 00:08:38 +0600

    адаптив слайдера

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,6 @@
 				slidesToScroll: 1,
 			 	arrows: false,
 				fade: true,
-				adaptiveHeight: true,
 				asNavFor: '.rooms-nav'
 			});
 			$('.rooms-nav').slick({
@@ -13,27 +12,8 @@
 				asNavFor: '.rooms-single',
 				dots: false,
 				arrows: true,
-				infinite: true,
 				focusOnSelect: true,
 				prevArrow: $('.left-arrow'),
 				nextArrow: $('.right-arrow')
 			});
 		});
-
-// $('.slider-for').slick({
-// 	slidesToShow: 1,
-// 	slidesToScroll: 1,
-// 	arrows: false,
-// 	fade: true,
-// 	asNavFor: '.slider-nav'
-// });
-// $('.slider-nav').slick({
-// 	slidesToShow: 3,
-// 	slidesToScroll: 1,
-// 	asNavFor: '.slider-for',
-// 	dots: false,
-// 	focusOnSelect: true,
-// 	arrows: true,
-// 	prevArrow: $('.prev'),
-// 	nextArrow: $('.next'),
-// });
