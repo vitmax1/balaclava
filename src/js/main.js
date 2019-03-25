c674888ee52cc7bed262c472e99243dfc2402de2
Виталий Максимов <2california@mail.ru>
2019-03-25 15:30:20 +0600

    бургер меню

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+	$('.menu__btn').click(function () {
+		$(".menu-list").toggleClass('menu-list__hide');	
+	});
+
+	$('.top-menu__btn').click(function () {
+		$(".top-menu-list").toggleClass('top-menu-list__hide');
+	});
+
+});
