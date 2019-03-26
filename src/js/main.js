caee7e9bbcac834047a8ce21b7a3fecd385a2023
Виталий Максимов <2california@mail.ru>
2019-03-27 02:06:34 +0600

    all

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
 
+	  var button = $('.button-active');
+	  var modal = $('#modal');
+	  var close = $('#close');
+
+	  button.on('click', function () {
+	  	modal.addClass('modal_active');
+	  });
+
+	  close.on('click', function () {
+	  	modal.removeClass('modal_active');
+	  });
+
 	$('.menu__btn').click(function () {
 		$(".menu-list").toggleClass('menu-list__hide');	
 	});
